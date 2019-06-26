9f339acd4e1c8ed92fc3c90ae2ca8abc4d55f860
shai <katzi0@gmail.com>
2019-06-26 20:45:29 +0300

    added apollo-cache

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,8 +1,10 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles';
 import TeamSelect from './TeamSelect/TeamSelect'
 import { Container } from '@material-ui/core'
+import  { gql } from 'apollo-boost'
+import { Mutation } from 'react-apollo';
 
 const useStyles = makeStyles(theme => ({
     button: {
@@ -13,16 +15,29 @@ const useStyles = makeStyles(theme => ({
     },
 }))
 
-const Header = () => {
+const TOGGLE_TODO = gql`
+  mutation ToggleTodo($id: Int!) {
+    toggleTodo(id: $id) @client
+  }
+`;
+
+const Header = ({ id, completed, text }) => {
     const classes = useStyles()
     return (
+        <Mutation mutation={TOGGLE_TODO} variables={{ id }}>
+    {toggleTodo => (
         <Container>
             <Button variant="contained" className={classes.button}>
                 Default
             </Button>
+            <span  onClick={toggleTodo}
+        style={{
+          textDecoration: completed ? 'line-through' : 'none',
+        }}>test</span>
             <TeamSelect />
         </Container>
-    )
-}
+    )}
+    </Mutation>
+    )}
 
 export default Header
